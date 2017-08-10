442c5224421b6d0288e599d04775da65df75e5a2
guodp <guodp9u0@gmail.com>
2017-08-10 18:46:35 +0800

    sava

--- underscore-1.8.3.js/demo/bind.js
+++ underscore-1.8.3.js/demo/bind.js
@@ -12,3 +12,25 @@ var func = _.bind(greet, { name: 'moe' }, 'hi');
 var test1 = func('!');
 var test2 = new func('!!!');
 console.log(`---------------------`);
+
+
+var fun = function() {
+
+}
+
+fun.prototype = {
+    info: {
+        name: 'peter',
+        age: 25
+    }
+}
+
+var a = new fun();
+var b = new fun();
+
+a.info.name = 'jack';
+b.info.name = 'tom';
+
+console.log(a.info.name);
+console.log(b.info.name);
+console.log(b.__proto__.info.name);
